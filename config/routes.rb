51a05b0c399b7ba9b45db311afa2bb505f5ec26f
Sindy Montes <sena2013@adsi-ThinkCentre-M77.(none)>
2014-08-05 13:56:48 -0500

    se borra un scaffold que no se necesitaba y se configura el archivo routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :proyects
+
   resources :engineers
+# esta es una relacion de uno a muchos
 
-  resources :people
-
-  resources :names
+  resources :proyects do
+    resources :engineers
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
